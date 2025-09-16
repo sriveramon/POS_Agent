577f51226880a07e99cdf0127f3a5db29ad805a5
Sebastian Rivera MAC <sebastian.rivera@gmail.com>
2025-09-16 17:22:58 -0600

    Standardizes Bluetooth MAC address format
    Updates the BluetoothConnection creation logic to use uppercase letters
    for the MAC address, ensuring consistency and alignment with typical
    standards for MAC address formatting.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from escpos import BluetoothConnection
 from escpos.impl.epson import GenericESCPOS
 
-conn = BluetoothConnection.create('66:32:30:73:ca:c8')
+conn = BluetoothConnection.create('66:32:30:73:CA:C8')
 printer = GenericESCPOS(conn)
 printer.init()
 printer.text('Hello World!')
